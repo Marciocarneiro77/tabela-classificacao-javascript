7e41cd971ad2e77342219994f7c618d88f06ed36
Marcio Carneiro 77 <mac.carneiro@yahoo.com.br>
2021-04-08 22:39:17 -0300

    atualização tabela javascript

---
+++ js/funcoes.js
@@ -0,0 +1,69 @@
+let guilherme = {
+    nome:"Guilherme",
+    vitoria:0,
+    empates:0,
+    derrotas:0,
+    pontos:0
+}
+
+let julia = {
+    nome:"Julia",
+    vitorias:0,
+    empates:0,
+    derrotas:0,
+    pontos:0
+}
+
+guilherme.pontos = CalculaPontos(guilherme);
+julia.pontos = CalculaPontos(julia);
+
+
+function CalculaPontos(jogador){
+    let pontos = (jogador.vitorias * 3) + jogador.empates;
+    return pontos;
+
+}
+let jogadores = [guilherme , julia];
+ExibirJogadoresNaTela(jogadores);
+
+function ExibirJogadoresNaTela(jogadores){
+    let html = "";
+    for(let i = 0 ; i < jogadores.length; i++){
+        html +="<tr><td>" + jogadores[i].nome + "</td>";
+        html += "<td>" + jogadores[i].vitorias + "</td>";
+        html += "<td>" + jogadores[i].empates + "</td>";
+        html += "<td>" + jogadores[i].derrotas + "</td>";
+        html += "<td>" + jogadores[i].pontos + "</td>";
+        html +="<td><button onclick = 'AdicionarVitoria(" + i +" )'>Vitória</button></td>";
+        html +="<td><button onclick = 'AdicionarEmpate()'>Empate</button></td>";
+        html +="<td><button onclick = 'AdicionarDerrota("+ i +")'>Derrota</button></td>";
+        html += "</tr>";
+
+        }
+        let tabelaJogadores = document.querySelector("#tabelaJogadores");
+        tabelaJogadores.innerHTML = html;
+
+}
+function AdicionarVitoria(i){
+    let jogador = jogadores[i];
+    jogador.vitorias++;
+    jogador.pontos = CalculaPontos(jogador);
+    ExibirJogadoresNaTela(jogadores);
+}
+function AdicionarEmpate(){
+    for (let i = 0; i<jogadores.length; i++){
+    
+    let jogador = jogadores[i];
+    jogador.empates++;
+    jogador.pontos = CalculaPontos(jogador);
+    ExibirJogadoresNaTela(jogadores);
+    }
+
+}
+function AdicionarDerrota(i){
+    let jogador = jogadores[i];
+    jogador.derrotas++;
+    ExibirJogadoresNaTela(jogadores);
+
+}
+
